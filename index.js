5a05a16cf5c3525d3edb9bdee0dc04fb9ca6bb69
Lake Chan <lakechan96@gmail.com>
2021-08-25 00:23:12 +0800

    Use promise

--- index.js
+++ index.js
@@ -1,47 +1,78 @@
 const http = require('https');
-const fs = require('fs');
-import * as exec from '@actions/exec';
+import * as fs from "fs/promise";
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 export const IS_WINDOWS = process.platform === 'win32';
 
-try {
-    // cd
-    if (IS_WINDOWS) {
-        fs.mkdir('C:\\Javalib', { recursive: true }, (err) => {
-            if (err) throw err;
-        });
-        process.chdir('C:\\Javalib');
-    } else {
-        process.chdir('/tmp');
-    }
+function download(url, dest) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(dest, { flags: "wx" });
 
-    const downloadUrl = core.getInput('download_url');
-    console.log(`Download from ${downloadUrl}`);
-    const file = fs.createWriteStream("antlr4.jar");
-    http.get(downloadUrl, function (response) {
-        response.pipe(file);
+        const request = http.get(url, response => {
+            if (response.statusCode === 200) {
+                response.pipe(file);
+            } else {
+                file.close();
+                fs.unlink(dest, () => {}); // Delete temp file
+                reject(`Server responded with ${response.statusCode}: ${response.statusMessage}`);
+            }
+        });
+
+        request.on("error", err => {
+            file.close();
+            fs.unlink(dest, () => {}); // Delete temp file
+            reject(err.message);
+        });
+
+        file.on("finish", () => {
+            resolve();
+        });
+
+        file.on("error", err => {
+            file.close();
+
+            if (err.code === "EEXIST") {
+                reject("File already exists");
+            } else {
+                fs.unlink(dest, () => {}); // Delete temp file
+                reject(err.message);
+            }
+        });
+    });
+}
+
+async function main(){
+    try {
+        const downloadUrl = core.getInput('download_url');
+        // mkdir & cd
+        if (IS_WINDOWS) {
+            await fs.mkdir('C:\\Javalib', { recursive: true });
+            process.chdir('C:\\Javalib');
+        } else {
+            process.chdir('/tmp');
+        }
+        // download
+        console.log(`Download from ${downloadUrl}`);
+        await download(downloadUrl, "antlr4.jar");
         // install
         if (IS_WINDOWS) {
             core.addPath("C:\\Javalib");
             core.exportVariable('CLASSPATH', ".;C:\\Javalib\\antlr4.jar;%CLASSPATH%");
-            fs.writeFileSync("antlr.bat", "java org.antlr.v4.Tool %*")
-            fs.writeFileSync("antlr4.bat", "java org.antlr.v4.Tool %*")
+            await fs.writeFile('antlr.bat', "java org.antlr.v4.Tool %*");
+            await fs.writeFile("antlr4.bat", "java org.antlr.v4.Tool %*")
         } else {
             core.addPath("/tmp");
             core.exportVariable('CLASSPATH', '.:/tmp/antlr4.jar:$CLASSPATH');
-            fs.writeFileSync("antlr", "!#/bin/sh -l\njava -Xmx500M -cp \"/tmp/antlr4.jar:$CLASSPATH\" org.antlr.v4.Tool")
-            fs.chmod("antlr", 0o775, (err) => {
-                if (err) throw err;
-                console.log('The permissions for file "my_file.txt" have been changed!');
-            });
+            await fs.writeFile("antlr", "!#/bin/sh -l\njava -Xmx500M -cp \"/tmp/antlr4.jar:$CLASSPATH\" org.antlr.v4.Tool")
+            await fs.chmod("antlr", 0o775);
         }
         console.log("Installed")
-    });
-
-} catch (error) {
-    console.log("Install failed")
-    console.trace(error);
-    core.setFailed(error.message);
+    } catch (error) {
+        console.log("Install failed")
+        console.trace(error);
+        core.setFailed(error.message);
+    }
 }
+
+main()
