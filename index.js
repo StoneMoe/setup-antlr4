0953029348ce7f6fb36dfa956e66759f83a33d39
Lake Chan <lakechan96@gmail.com>
2021-08-24 22:26:51 +0800

    fix download

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import JsFileDownloader from 'js-file-downloader';
+const http = require('https');
+const fs = require('fs');
 import * as exec from '@actions/exec';
-import { chmod, writeFileSync } from 'fs';
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -16,28 +16,22 @@ try {
 
     const downloadUrl = core.getInput('download_url');
     console.log(`Download from ${downloadUrl}`);
-    new JsFileDownloader({
-        url: downloadUrl,
-        filename: "antlr4.jar"
-    }).then(function () {
-        // install
-        if (IS_WINDOWS) {
-            core.addPath("C:\\Javalib");
-            core.exportVariable('CLASSPATH', ".;C:\\Javalib\\antlr4.jar;%CLASSPATH%");
-            writeFileSync("antlr.bat", "java org.antlr.v4.Tool %*")
-            writeFileSync("antlr4.bat", "java org.antlr.v4.Tool %*")
-        } else {
-            core.addPath("/usr/local/lib");
-            core.exportVariable('CLASSPATH', '.:/usr/local/lib/antlr4.jar:$CLASSPATH');
-            writeFileSync("antlr", "!#/bin/sh -l\njava -Xmx500M -cp \"/usr/local/lib/antlr4.jar:$CLASSPATH\" org.antlr.v4.Tool")
-            chmod("antlr", "777")
-        }
-        
-    }).catch(function (error) {
-        console.trace(error);
-        core.setFailed(error.message);
+    const file = fs.createWriteStream("antlr4.jar");
+    http.get(downloadUrl, function(response) {
+        response.pipe(file);
     });
-
+    // install
+    if (IS_WINDOWS) {
+        core.addPath("C:\\Javalib");
+        core.exportVariable('CLASSPATH', ".;C:\\Javalib\\antlr4.jar;%CLASSPATH%");
+        fs.writeFileSync("antlr.bat", "java org.antlr.v4.Tool %*")
+        fs.writeFileSync("antlr4.bat", "java org.antlr.v4.Tool %*")
+    } else {
+        core.addPath("/usr/local/lib");
+        core.exportVariable('CLASSPATH', '.:/usr/local/lib/antlr4.jar:$CLASSPATH');
+        fs.writeFileSync("antlr", "!#/bin/sh -l\njava -Xmx500M -cp \"/usr/local/lib/antlr4.jar:$CLASSPATH\" org.antlr.v4.Tool")
+        fs.chmod("antlr", "777")
+    }
 } catch (error) {
     console.trace(error);
     core.setFailed(error.message);
